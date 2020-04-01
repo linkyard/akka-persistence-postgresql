e785858c8145f1cd789d4f5fb2a8506e6ad73437
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2020-04-01 11:11:16 +0200

    chore: update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
 
-  val akkaVersion    = "2.6.0"
+  val akkaVersion    = "2.6.4"
   val slickVersion   = "3.3.2"
   val gatlingVersion = "2.2.4"
 
-  val scalaJava8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0"
+  val scalaJava8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.1"
 
   val akkaPersistence      = "com.typesafe.akka" %% "akka-persistence"       % akkaVersion
   val akkaSlf4j            = "com.typesafe.akka" %% "akka-slf4j"             % akkaVersion
@@ -21,14 +21,14 @@ object Dependencies {
   val scalaTest              = "org.scalatest"     %% "scalatest"            % "3.0.8"     % "test,it"
   val akkaTest               = "com.typesafe.akka" %% "akka-testkit"         % akkaVersion % "test,it"
   val akkaPersistenceTestkit = "com.typesafe.akka" %% "akka-persistence-tck" % akkaVersion % "test,it"
-  val slf4jSimple            = "org.slf4j"         % "slf4j-simple"          % "1.7.26"    % "test,it"
+  val slf4jSimple            = "org.slf4j"         % "slf4j-simple"          % "1.7.30"    % "test,it"
   val tyrex                  = "tyrex"             % "tyrex"                 % "1.0.1"     % "test,it"
 
   // gatling benchmark module
   val gatlinHighcharts = "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion
   val gatling          = "io.gatling"            % "gatlin"                    % gatlingVersion
 
-  val postgres = "org.postgresql" % "postgresql" % "42.2.6"
+  val postgres = "org.postgresql" % "postgresql" % "42.2.12"
 
   val mainTestDependencies = Seq(
     scalaTest,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,3 +8,5 @@ addSbtPlugin("com.jsuereth"   % "sbt-pgp"      % "1.1.2")
 addSbtPlugin("com.dwijnand"   % "sbt-travisci" % "1.1.3")
 addSbtPlugin("com.dwijnand"   % "sbt-dynver"   % "3.3.0")
 addSbtPlugin("org.scalameta"  % "sbt-scalafmt" % "2.0.1")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.5.0")
